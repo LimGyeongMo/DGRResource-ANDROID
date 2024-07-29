9cd9d46eb6d25e84ce03b45abe401e51d6a22265
LimGyeongMo <133728406+LimGyeongMo@users.noreply.github.com>
2024-07-29 15:12:35 +0900

    12

---
+++ app/src/main/java/com/project/retest/testUtil.java
@@ -0,0 +1,21 @@
+package com.project.retest;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class testUtil {
+    private Context context;
+
+    private static Toast toast;
+
+    public testUtil(Context context) {
+        this.context = context;
+    }
+
+    public void showToast(String msg) {
+        Toast myToast = Toast.makeText(context,msg, Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+}
